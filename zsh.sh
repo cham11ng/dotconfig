e6bac405bc983bc1f757ed055e007ba6bb08c126
Sagar Chamling <sgr.raee@gmail.com>
2018-12-25 20:32:25 +0545

    Update zsh scripts

--- zsh.sh
+++ zsh.sh
@@ -2,7 +2,7 @@ sudo apt install zsh
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-curl https://raw.githubusercontent.com/shashankmehta/dotfiles/master/thesetup/zsh/.oh-my-zsh/custom/themes/gitster.zsh-theme >> ~/.oh-my-zsh/themes
+curl -X https://raw.githubusercontent.com/shashankmehta/dotfiles/master/thesetup/zsh/.oh-my-zsh/custom/themes/gitster.zsh-theme >> ~/.oh-my-zsh/themes/gitster.zsh-theme
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
