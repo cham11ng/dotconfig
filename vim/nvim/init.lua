65ab49670df391c86222e77592e4bf10c2be3580
Sagar Chamling <sgr.raee@gmail.com>
2026-06-06 01:26:36 -0400

    Configure Vim options for line numbers and indentation (#3)

--- vim/nvim/init.lua
+++ vim/nvim/init.lua
@@ -6,3 +6,8 @@ vim.keymap.set('i', 'kj', '<Esc>', {noremap = true, silent = true})
 -- Optional: Map to <Esc> in visual mode as well
 vim.keymap.set('v', 'jk', '<Esc>', {noremap = true, silent = true})
 vim.keymap.set('v', 'kj', '<Esc>', {noremap = true, silent = true})
+
+vim.opt.number = true          -- Show line numbers
+vim.opt.relativenumber = true  -- Relative line numbers
+vim.opt.shiftwidth = 4         -- Size of an indent
+vim.opt.expandtab = true       -- Use spaces instead of tabs
